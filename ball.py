e1acf4f56afaa36246b8905d2c281bee7000f67e
YahavBoneh <59557433+YahavBoneh@users.noreply.github.com>
2020-10-14 12:26:55 +0300

    Added randomness after collision with paddles. Reformatted Ball functions a little bit.

--- ball.py
+++ ball.py
@@ -41,16 +41,51 @@ class Ball:
 
         self.speed_factor = self.settings.ball.speed_factor
 
+    def __collide_top_bottom_edges(self, side):
+        """
+        :param side: "top" or "bottom".
+        """
+        if side == "top":
+            sign = 1
+        elif side == "bottom":
+            sign = -1
+        else:
+            return
+        self.direction[1] = sign * abs(self.direction[1])  # Can't just multiply by -1 so the ball won't get stuck if the collision is for more than one frame.
+
+    def __collide_paddles(self, side):
+        """
+        :param side: "left" or "right"
+        """
+        if side == "left":
+            sign = 1
+        elif side == "right":
+            sign = -1
+        else:
+            return
+        self.direction[0] = sign * abs(self.direction[0])
+        random_vector = random_normalized_vector(self.settings.ball.max_first_slope)
+        random_vector = [sign * abs(random_vector[0]), random_vector[1]]
+        self.direction = normalize([self.direction[0] + self.settings.ball.randomness_weight * random_vector[0],
+                                    self.direction[1] + self.settings.ball.randomness_weight * random_vector[1]])
+
+    def __return_to_middle(self):
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.centery = self.screen_rect.centery
+        self.float_centerx = float(self.rect.centerx)
+        self.float_centery = float(self.rect.centery)
+        self.direction = random_normalized_vector(self.settings.ball.max_first_slope)
+
     def update(self):
         if self.rect.top < 0:
-            self.direction[1] = abs(self.direction[1])
+            self.__collide_top_bottom_edges("top")
         elif self.rect.bottom > self.screen_rect.bottom:
-            self.direction[1] = -abs(self.direction[1])
+            self.__collide_top_bottom_edges("bottom")
 
         if self.rect.colliderect(self.left_paddle_rect) and self.rect.left >= self.left_paddle_rect.left:
-            self.direction[0] = abs(self.direction[0])
+            self.__collide_paddles("left")
         elif self.rect.colliderect(self.right_paddle_rect) and self.rect.right <= self.right_paddle_rect.right:
-            self.direction[0] = -abs(self.direction[0])
+            self.__collide_paddles("right")
 
         if self.rect.left < 0 or self.rect.right > self.screen_rect.right:
             if self.rect.left < 0:
@@ -58,11 +93,7 @@ class Ball:
             else:
                 self.score_board.left_score += 1
 
-            self.rect.centerx = self.screen_rect.centerx
-            self.rect.centery = self.screen_rect.centery
-            self.float_centerx = float(self.rect.centerx)
-            self.float_centery = float(self.rect.centery)
-            self.direction = random_normalized_vector(self.settings.ball.max_first_slope)
+            self.__return_to_middle()
             return
 
         self.float_centerx += self.direction[0] * self.speed_factor
--- pong_settings.py
+++ pong_settings.py
@@ -16,11 +16,12 @@ class PaddleSettings:
 
 
 class BallSettings:
-    def __init__(self, radius, picture_path, speed_factor, max_first_slope):
+    def __init__(self, radius, picture_path, speed_factor, max_first_slope, randomness_weight):
         self.radius = radius
         self.picture_path = picture_path
         self.speed_factor = speed_factor
         self.max_first_slope = max_first_slope
+        self.randomness_weight = randomness_weight
 
 
 class ScoreBoardSettings:
@@ -34,5 +35,6 @@ class PongSettings:
     def __init__(self):
         self.screen = ScreenSettings(width=600, height=400, color=(0, 0, 0))
         self.paddle = PaddleSettings(width=10, height=60, distance_from_edge=30, color=(250, 250, 250), speed_factor=0.5)
-        self.ball = BallSettings(radius=10, picture_path="Figures/ball.bmp", speed_factor=0.25, max_first_slope=1)
+        self.ball = BallSettings(radius=10, picture_path="Figures/ball.bmp", speed_factor=0.25, max_first_slope=1,
+                                 randomness_weight=0.3)
         self.score_board = ScoreBoardSettings(text_color=(250, 250, 250), font_size=48, space_between_scores=5)
